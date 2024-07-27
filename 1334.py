64b5ff0941fe05c9c1a97e59b3a969dd69970299
Hayatokane <cheungkuensum@gmail.com>
2024-07-27 10:48:19 +0800

    Finish 1334

--- 1334.py
+++ 1334.py
@@ -2,58 +2,63 @@ import heapq
 from typing import List
 
 
-class Node:
-    def __init__(self, index, parent, distance):
-        self.index = index
-        self.distance = distance
-        self.parent = parent
-
-    def __lt__(self, other):
-        return self.distance < other.distance
-
-
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
-        queues, adjacent_lists = self.initialise(n, edges, distanceThreshold)
+        adjacent_lists = self.initialise(n, edges)
+        source = 0
+        min_count = n + 1
         for i in range(n):
-            self.dijkstra(n, edges, i, queues, distanceThreshold, adjacent_lists)
+            parent = self.dijkstra(n, i, distanceThreshold, adjacent_lists)
+            count = self.count_parent(parent)
+            if count <= min_count:
+                min_count = count
+                source = i
+        return source
 
-    def initialise(self, n: int, edges: List[List[int]], distanceThreshold: int):
-        """We loop through the edges once to build our initial queues"""
-        queues = []
+    def initialise(self, n: int, edges: List[List[int]]):
         adjacent_lists = []
         for i in range(n):
-            queue = []
-            for j in range(n):
-                if i == j:
-                    heapq.heappush(queue,
-                               Node(j, -1, 0))
-                else:
-                    heapq.heappush(queue, Node(j, -1, distanceThreshold + 1))
+            adjacent_lists.append([])
+        for edge in edges:
+            # edge is [startnode, endnode, weight]
+            startnode = edge[0]
+            endnode = edge[1]
+            weight = edge[2]
+            adjacent_lists[startnode].append((endnode, weight))
+            adjacent_lists[endnode].append((startnode, weight))
+        return adjacent_lists
 
-                queues.append(queue)
-                adjacent_lists.append([])
-
-                for edge in edges:
-                # edge is [startnode, endnode, weight]
-                    startnode = edge[0]
-                endnode = edge[1]
-                weight = edge[2]
-                queues[startnode][endnode] = (-1, weight)
-                adjacent_lists[startnode].append(endnode)
-        return queues, adjacent_lists
-
-    def dijkstra(self, n: int, edges: List[List[int]], source: int, queues, distanceThreshold: int, adjacent_lists):
+    def dijkstra(self, n: int, source: int, distanceThreshold: int, adjacent_lists: List[List[int]]) -> List[int]:
+        parent = []
         mins = []
         for i in range(n):
-            mins.append(-1, -1)
+            parent.append(-1)
+            if source != i:
+                mins.append(distanceThreshold + 1)
+            else:
+                mins.append(0)
         """if node 0 is the source, initital queue would be 
         [(0,-1,0),(1,-1, 2),(2,-1, inf), (3,-1, inf), (4,-1, 8)]"""
-        queue = queues[source]
+        queue = [(mins[i], i) for i in range(n)]
+        heapq.heapify(queue)
         while queue:
-            node = heapq.heappop(queue)
-            for adj_node in adjacent_lists[node.index]:
-                if mins[adj_node] > node.distance+ :
+            node = heapq.heappop(queue)[1]
+            for adj_node in adjacent_lists[node]:
+                w = mins[node] + adj_node[1]
+                if mins[adj_node[0]] > w and w <= distanceThreshold:
+                    mins[adj_node[0]] = w
+                    parent[adj_node[0]] = node
+                    heapq.heappush(queue, (w, adj_node[0]))
+        return parent
 
-
-n = 4, edges = [[0, 1, 3], [1, 2, 1], [1, 3, 4], [2, 3, 1]], distanceThreshold = 4
+    def count_parent(self, parent:List[int]):
+        count = 0
+        for node in parent:
+            if node != -1:
+                count += 1
+        return count
+# n = 5
+# edges = [[0, 1, 2], [0, 4, 8], [1, 2, 3], [1, 4, 2], [2, 3, 1], [3, 4, 1]]
+# distanceThreshold = 2
+# solution = Solution()
+# print(solution.findTheCity(n, edges, distanceThreshold))
