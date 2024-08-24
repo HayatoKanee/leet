4c38f07f88842888b92758d397d078156a740240
Hayatokane <cheungkuensum@gmail.com>
2024-08-24 12:16:47 +0800

    Finish 2698

---
+++ 2698.py
@@ -0,0 +1,27 @@
+from itertools import combinations
+
+
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        punishment = 0
+        for i in range(10, 11):
+            punishment += self.punishment(0, str(i * i), i)
+        return punishment
+
+    def punishment(self, index, num_str, target):
+        if target == 0:
+            return int(num_str)
+        if target < 0:
+            return 0
+        if index == len(num_str):
+            return 0
+        for i in range(index, len(num_str)):
+            punishment = self.punishment(index + 1, num_str, target - int(num_str[index: i + 1]))
+            if punishment:
+                return punishment
+        return 0
+
+
+n = 10
+sol = Solution()
+print(sol.punishmentNumber(n))
