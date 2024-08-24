5df311bae03b57c45b00db72839582c206f84610
Hayatokane <cheungkuensum@gmail.com>
2024-08-24 12:33:23 +0800

    Finish 2698

--- 2698.py
+++ 2698.py
@@ -1,27 +1,14 @@
-from itertools import combinations
-
 
 class Solution:
     def punishmentNumber(self, n: int) -> int:
         punishment = 0
-        for i in range(n):
-            punishment += self.punishment(0, str(i * i), i)
+        for i in range(1,n+1):
+            if self.punishment(0,0, str(i * i), i):
+                punishment += i*i
         return punishment
 
-    def punishment(self, index, num_str, target):
-        if target == 0:
-            return int(num_str)
-        if target < 0:
-            return 0
-        if index == len(num_str):
-            return 0
-        for i in range(index, len(num_str)):
-            punishment = self.punishment(index + 1, num_str, target - int(num_str[index: i + 1]))
-            if punishment:
-                return punishment
-        return 0
-
-
-n = 10
-sol = Solution()
-print(sol.punishmentNumber(n))
+    def punishment(self, added,cached_sum,num_str, target):
+        if not num_str:
+            return target == added + cached_sum
+        num = int(num_str[0])
+        return self.punishment(added,cached_sum*10+num,num_str[1:], target) or self.punishment(added+num,cached_sum,num_str[1:], target)
